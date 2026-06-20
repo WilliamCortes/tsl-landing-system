a3bfb5888eeaec9f203825bd58438220da50d66f
WilliamCortes <williamcortesro@outlook.es>
2026-06-20 15:19:15 -0500

    fix: time counter

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -68,9 +68,6 @@ export default function Header() {
                     aria-expanded={desktopDropdownOpen}
                   >
                     {item.label}
-                    <svg viewBox="0 0 20 20" fill="currentColor" className="ml-1 h-3 w-3">
-                      <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
-                    </svg>
                   </button>
                   {desktopDropdownOpen && (
                     <div className="absolute left-0 top-full z-50 min-w-[180px] border-t-[3px] border-[var(--color-brand-blue)] bg-white shadow-[0_2px_6px_rgba(0,0,0,0.1)]">
